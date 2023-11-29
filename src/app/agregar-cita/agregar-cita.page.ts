17618b87e02f924d40dc92c325e732c917b84f53
Lluan12 <103333263+Lluan12@users.noreply.github.com>
2023-11-29 01:10:35 -0700

    agregar Cita primera version

--- src/app/agregar-cita/agregar-cita.page.ts
+++ src/app/agregar-cita/agregar-cita.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-cita',
   templateUrl: './agregar-cita.page.html',
   styleUrls: ['./agregar-cita.page.scss'],
 })
-export class AgregarCitaPage implements OnInit {
+export class AgregarCitaPage {
 
-  constructor() { }
+  citaForm: FormGroup;
+  pacientes: any[] = [
+    {
+      id: 1,
+      nombre: 'Juan Pérez'
+    },
+    {
+      id: 2,
+      nombre: 'María López'
+    },
+    {
+      id: 3,
+      nombre: 'Pedro Martínez'
+    }
+  ];
+  opcion: string = this.pacientes[0].nombre;
+  estatus: any[] = [
+    {
+      id: 1,
+      nombre: 'Pendiente'
+    },
+    {
+      id: 2,
+      nombre: 'Cancelada'
+    },
+    {
+      id: 3,
+      nombre: 'Confirmada'
+    }
+  ];
+  opcionEstatus: string = this.estatus[0].nombre;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) { 
+    this.citaForm = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      nombre: ['', Validators.required],
+      estatus: ['', Validators.required],
+      fecha: ['', Validators.required],
+      hora: ['', Validators.required],
+      descripcion: ['', Validators.required]
+    });  
   }
 
 }
