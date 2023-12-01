b6cefd42d9b6023e324311d3f412adb1469f01f1
IvanRobles19 <107086571+IvanRobles19@users.noreply.github.com>
2023-12-01 15:04:39 -0700

    Patient list with search engine (interface)

---
+++ src/app/models/paciente.model.ts
@@ -0,0 +1,7 @@
+export interface Paciente {
+    id? : number;
+    name: string;
+    edad: number;
+    suffering: string;
+    date : Date;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { IonInput } from '@ionic/angular';
+import { Paciente } from '../models/paciente.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +10,34 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  public data: Paciente[] = [
+    { id: 1, name: 'John Doe', edad: 25, suffering: 'Dolor de Espalda', date: new Date('2023-11-06T09:00:00') },
+    { id: 2, name: 'Alice Smith', edad: 30, suffering: 'Dolor de Cabeza', date: new Date('2023-11-07T10:00:00') },
+    { id: 3, name: 'Carlos Pérez', edad: 40, suffering: 'Fiebre', date: new Date('2023-11-08T11:00:00') },
+    { id: 4, name: 'Ana Gómez', edad: 35, suffering: 'Problemas Estomacales', date: new Date('2023-11-09T12:00:00') },
+    { id: 5, name: 'Javier López', edad: 28, suffering: 'Migraña', date: new Date('2023-11-10T13:00:00') },
+    
+  ];
+
+  public results: Paciente[] = [...this.data];
+
+  handleInput(event: any) {
+    const inputValue = event.target.value;
+
+    if (inputValue && inputValue.trim() !== '') {
+      const query = inputValue.toLowerCase();
+
+      this.results = this.data.filter((paciente) =>
+        paciente.name.toLowerCase().includes(query) ||
+        String(paciente.edad).includes(query) ||
+        paciente.date.toISOString().includes(query)
+      );
+    } else {
+      // Si el valor es nulo, undefined o una cadena vacía, restaura al estado original
+      this.results = [...this.data];
+    }
+  }
+
   constructor(private router: Router, private authService: AuthService) {}
 
   public logout() {
