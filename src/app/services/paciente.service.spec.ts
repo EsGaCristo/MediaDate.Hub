1c5de3527ef98d233fa601ac60a607fa82b2eca4
paul <paalochoame@ittepic.edu.mx>
2023-12-10 18:53:02 -0700

    colordetoolbar

--- src/app/services/paciente.service.spec.ts
+++ src/app/services/paciente.service.spec.ts
@@ -5,10 +5,10 @@ import { PacienteService } from './paciente.service';
 describe('PacienteService', () => {
   let service: PacienteService;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PacienteService);
-  });
+  }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
